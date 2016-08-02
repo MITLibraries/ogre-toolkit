a43602e4f731934e0eb1693f3e290ef1c06996b0
Mike Graves <mgraves@mit.edu>
2016-08-02 13:17:50 -0400

    Update to geoblacklight schema 1.0
    This updates the record to reflect changes to the schema. This also
    replaces the use of descriptors with properties. The properties are
    easier to manage and more flexible.

--- ogre/exceptions.py
+++ ogre/exceptions.py
@@ -3,6 +3,5 @@
 
 class InvalidDataError(Exception):
     def __init__(self, field, value):
-        super(InvalidDataError, self).__init__(field, value)
         self.field = field
         self.value = value
--- ogre/record.py
+++ ogre/record.py
@@ -1,140 +1,160 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-import arrow
 import json
-from six import add_metaclass
 
-from ogre.fields import (RecordMeta, String, Enum, Dictionary, DateTime, Set,
-                         Integer, Decimal)
+from ogre.fields import Enum, optional
 
 
-@add_metaclass(RecordMeta)
-class BaseRecord(object):
-    pass
-
-
-class Record(BaseRecord):
-    """Defines a GeoBlacklight record.
-
-    A record's fields (see https://github.com/geoblacklight/geoblacklight-schema)
-    can be initialized through keyword arguments or set later::
-
-        from ogre.record import Record
-
-        record = Record(dc_title_s=u'Cambridge Libraries')
-        record.dct_provenance_s = u'MIT'
-
-    This class can be easily subclassed to create a specific kind of record::
-
-        from ogre.record import Record
-        from ogre.fields import String, Enum
-
-        class MitRecord(Record):
-            dct_provenance_s = String(default=u'MIT')
-            dc_rights_s = Enum(enums=[u'Public', u'Restricted'],
-                               default=u'Restricted')
-
-    :param kwargs: initial field values
-    """
-
-    uuid = String()
-    dc_identifier_s = String()
-    dc_title_s = String()
-    dc_description_s = String()
-    dc_rights_s = Enum(enums=['Public', 'Restricted'])
-    dct_provenance_s = String()
-    dct_references_s = Dictionary()
-    layer_id_s = String()
-    layer_geom_type_s = Enum(enums=['Point', 'Line', 'Polygon', 'Raster',
-                                'Scanned Map', 'Paper Map', 'Mixed'])
-    layer_modified_dt = DateTime(default=arrow.now())
-    layer_slug_s = String()
-    solr_year_i = Integer()
-    dc_creator_sm = Set()
-    dc_format_s = String()
-    dc_language_s = String()
-    dc_publisher_s = String()
-    dc_subject_sm = Set()
-    dc_type_s = Enum(enums=['Dataset', 'Image', 'PhysicalObject'])
-    dct_spatial_sm = Set()
-    dct_temporal_sm = Set()
-    dct_issued_dt = String()
-    dct_isPartOf_sm = Set()
-
-    _bbox_w = Decimal()
-    _bbox_n = Decimal()
-    _bbox_e = Decimal()
-    _bbox_s = Decimal()
-    _lat = Decimal()
-    _lon = Decimal()
+class Record(object):
+    dc_description_s = None
+    dc_identifier_s = None
+    dc_language_s = None
+    dc_publisher_s = None
+    dc_title_s = None
+    dct_issued_dt = None
+    dct_provenance_s = None
+    geoblacklight_version = None
+    layer_id_is = None
+    layer_modified_dt = None
+    layer_slug_s = None
 
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
             setattr(self, k, v)
 
     @property
-    def georss_box_s(self):
-        bounds = (self._bbox_s, self._bbox_w, self._bbox_n, self._bbox_e)
-        if None in bounds:
-            return None
-        return "%s %s %s %s" % bounds
+    @optional
+    def dc_creator_sm(self):
+        return self._dc_creator_sm
+
+    @dc_creator_sm.setter
+    def dc_creator_sm(self, value):
+        self._dc_creator_sm = set(value)
+
+    @property
+    @optional
+    def dc_format_s(self):
+        return self._dc_format_s
+
+    @dc_format_s.setter
+    @Enum('Shapefile', 'GeoTiff', 'ArcGRID')
+    def dc_format_s(self, value):
+        self._dc_format_s = value
+
+    @property
+    def dc_rights_s(self):
+        return self._dc_rights_s
+
+    @dc_rights_s.setter
+    @Enum('Public', 'Restricted')
+    def dc_rights_s(self, value):
+        self._dc_rights_s = value
+
+    @property
+    @optional
+    def dc_source_sm(self):
+        return self._dc_source_sm
+
+    @dc_source_sm.setter
+    def dc_source_sm(self, value):
+        self._dc_source_dm = set(value)
+
+    @property
+    @optional
+    def dc_subject_sm(self):
+        return self._dc_subject_sm
+
+    @dc_subject_sm.setter
+    def dc_subject_sm(self, value):
+        self._dc_subject_sm = set(value)
+
+    @property
+    @optional
+    def dc_type_s(self):
+        return self._dc_type_s
+
+    @dc_type_s.setter
+    @Enum('Dataset', 'Image', 'PhysicalObject')
+    def dc_type_s(self, value):
+        self._dc_type_s = value
+
+    @property
+    @optional
+    def dct_isPartOf_sm(self):
+        return self._dct_isPartOf_sm
+
+    @dct_isPartOf_sm.setter
+    def dct_isPartOf_sm(self, value):
+        self._dct_isPartOf_sm = set(value)
+
+    @property
+    def dct_references_s(self):
+        return self._dct_references_s
+
+    @dct_references_s.setter
+    def dct_references_s(self, value):
+        self._dct_references_s = dict(value)
+
+    @property
+    @optional
+    def dct_spatial_sm(self):
+        return self._dct_spatial_sm
+
+    @dct_spatial_sm.setter
+    def dct_spatial_sm(self, value):
+        self._dct_spatial_sm = set(value)
+
+    @property
+    @optional
+    def dct_temporal_sm(self):
+        return self._dct_temporal_sm
+
+    @dct_temporal_sm.setter
+    def dct_temporal_sm(self, value):
+        self._dct_temporal_sm = set(value)
+
+    @property
+    def layer_geom_type_s(self):
+        return self._layer_geom_type_s
+
+    @layer_geom_type_s.setter
+    @Enum('Point', 'Line', 'Polygon', 'Raster', 'Scanned Map', 'Mixed')
+    def layer_geom_type_s(self, value):
+        self._layer_geom_type_s = value
 
     @property
     def solr_geom(self):
-        bounds = (self._bbox_w, self._bbox_e, self._bbox_n, self._bbox_s)
-        if None in bounds:
-            return None
-        return "ENVELOPE(%s, %s, %s, %s)" % bounds
+        return self._solr_geom
 
-    @property
-    def georss_point_s(self):
-        point = (self._lat, self._lon)
-        if None in point:
-            return None
-        return "%s %s" % point
+    @solr_geom.setter
+    def solr_geom(self, values):
+        """W,E,N,S"""
+        self._solr_geom = "ENVELOPE({}, {}, {}, {})".format(*values)
 
-    def as_dict(self):
-        """Return record as a dictionary.
-
-        This dictionary will contain only fields present in the current
-        version of the GeoBlacklight schema.
-
-        The ``dct_references_s`` field, if not ``None``, will be
-        represented as a valid JSON string.
-
-        :rtype: dictionary
-        """
-
-        if self.dct_references_s:
-            references = json.dumps(self.dct_references_s)
-        else:
-            references = None
-        d = {
-            'uuid': self.uuid,
-            'dc_identifier_s': self.dc_identifier_s,
-            'dc_title_s': self.dc_title_s,
+    def to_json(self):
+        record = {
+            'dc_creator_sm': list(self.dc_creator_sm or []),
             'dc_description_s': self.dc_description_s,
+            'dc_format_s': self.dc_format_s,
+            'dc_identifier_s': self.dc_identifier_s,
+            'dc_language_s': self.dc_language_s,
+            'dc_publisher_s': self.dc_publisher_s,
             'dc_rights_s': self.dc_rights_s,
+            'dc_source_sm': list(self.dc_source_sm or []),
+            'dc_subject_sm': list(self.dc_subject_sm or []),
+            'dc_title_s': self.dc_title_s,
+            'dc_type_s': self.dc_type_s,
+            'dct_isPartOf_sm': list(self.dct_isPartOf_sm or []),
+            'dct_issued_dt': self.dct_issued_dt,
             'dct_provenance_s': self.dct_provenance_s,
-            'dct_references_s': references,
-            'georss_box_s': self.georss_box_s,
-            'layer_id_s': self.layer_id_s,
+            'dct_references_s': self.dct_references_s,
+            'dct_spatial_sm': list(self.dct_spatial_sm or []),
+            'dct_temporal_sm': list(self.dct_temporal_sm or []),
+            'geoblacklight_version': self.geoblacklight_version,
             'layer_geom_type_s': self.layer_geom_type_s,
+            'layer_id_s': self.layer_id_s,
             'layer_modified_dt': self.layer_modified_dt,
             'layer_slug_s': self.layer_slug_s,
             'solr_geom': self.solr_geom,
-            'solr_year_i': self.solr_year_i,
-            'dc_creator_sm': self.dc_creator_sm,
-            'dc_format_s': self.dc_format_s,
-            'dc_language_s': self.dc_language_s,
-            'dc_publisher_s': self.dc_publisher_s,
-            'dc_subject_sm': self.dc_subject_sm,
-            'dc_type_s': self.dc_type_s,
-            'dct_spatial_sm': self.dct_spatial_sm,
-            'dct_temporal_sm': self.dct_temporal_sm,
-            'dct_issued_dt': self.dct_issued_dt,
-            'dct_isPartOf_sm': self.dct_isPartOf_sm,
-            'georss_point_s': self.georss_point_s,
         }
-
-        return dict((k, v) for k, v in d.items() if v)
+        return json.dumps({k: v for k,v in record.items() if v})
